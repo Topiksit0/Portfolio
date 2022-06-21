9171bd675c06811aa7eea2d5bb6e96c932da767d
Topiksit0 <marcospalominovivar@gmail.com>
2022-06-21 14:26:44 +0200

    home animations

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,8 +7,18 @@ const Home = () => {
 
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
             <p>--- Hello world, I'm</p>
-            <h1> Marcus Vivar</h1>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#D5A82C]'> Marcus Vivar</h1>
             <p>Frontend developer</p>
+
+            <div>
+                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#D5A82C] hover:border-[#D5A82C] hover:text-black '> 
+                Contact me  
+                <span className='group-hover:rotate-90 duration-300'>
+                  <HiArrowNarrowRight className='ml-3'/>
+                </span>
+                
+                </button>
+            </div>
 
         </div>
 
