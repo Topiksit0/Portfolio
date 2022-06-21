897b4030c243bd221bb5a99ab3a1dae3b785ffd2
Topiksit0 <marcospalominovivar@gmail.com>
2022-06-21 14:47:19 +0200

    landing page 60% completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,13 +6,12 @@ const Home = () => {
     <div name='home' className='w-full h-screen bg-[#2B2727]'>
 
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p>--- Hello world, I'm</p>
-            <h1 className='text-4xl sm:text-7xl font-bold text-[#D5A82C]'> Marcus Vivar</h1>
-            <p>Frontend developer</p>
-
-            <div>
-                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#D5A82C] hover:border-[#D5A82C] hover:text-black '> 
-                Contact me  
+            <p className='text-md sm:text-xl text-gray-300'>--- Hello world, I'm</p>
+            <h1 className='text-5xl sm:text-8xl font-bold text-[#D5A82C] tracking-tight'> Marcus Vivar</h1>
+            <p className='text-md sm:text-xl text-gray-300 italic'>Problem solver and Front-end Developer</p>
+            <div className='pt-10'>
+                <button className='text-white group border-2 px-6 py-3 my-2  flex items-center hover:bg-[#D5A82C] hover:border-[#D5A82C] hover:text-black'> 
+                Contact Me  
                 <span className='group-hover:rotate-90 duration-300'>
                   <HiArrowNarrowRight className='ml-3'/>
                 </span>
