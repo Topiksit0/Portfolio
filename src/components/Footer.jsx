9583e3b3f3874e770858231fa8ed2a73277df13d
Topiksit0 <marcospalominovivar@gmail.com>
2022-06-23 12:23:04 +0200

    footer prob solv

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
   return (
     <div className='bg-[#2B2727] flex flex-col justify-center items-center w-full h-full top'>
         <div>
-         <img src={logo} className="max-w-[120px] mt-24"   />
+         <img src={logo} className="sm:max-w-[120px] max-w-[90px] mt-24"   />
         </div>
         
-        <div className='max-w-[500px] w-full px-4 grid grid-cols-3 mt-8 justify-center'>
+        <div className='max-w-[500px]  w-full px-4 grid grid-cols-3 mt-8 justify-center'>
             <div className='flex justify-center items-center'>
                 <a href='https://twitter.com/topiksit0' className='text-white font-extrabold underL text-center'>Twitter</a>
             </div>
@@ -21,7 +21,7 @@ const Footer = () => {
         </div>
 
         <div className='mt-24 text-white mb-6'>
-            <p>© 2022 Marcus Vivar - Designed and built by <strong> Marcus</strong></p>
+            <p>© 2022 Marcus Vivar - Designed and built by <strong> Marcus.</strong></p>
         </div>
         
     </div>
