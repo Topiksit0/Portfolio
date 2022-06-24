1c2b02ee52b9d2a715e9dd05164d94a28a4e722e
Topiksit0 <marcospalominovivar@gmail.com>
2022-06-24 16:43:46 +0200

    No se que he hecho

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
 import {Link} from 'react-scroll'
-
+import Typical from 'react-typical'
 
 const Home = () => {
   return (
@@ -13,7 +13,15 @@ const Home = () => {
              Hello world, I'm
             </p>
             <h1 className='text-5xl sm:text-8xl font-bold text-[#D5A82C] tracking-tight'> Marcus Vivar.</h1>
-            <p className='text-md sm:text-xl text-gray-300 italic'>Problem solver and Front-end Developer</p>
+            <p className='text-md sm:text-xl text-gray-300 italic'>
+              <Typical 
+                steps ={['Problem solver and Front-end Developer',1000]}
+                loop={Infinity}
+                wrapper="p"
+              />
+
+            </p>
+            
             <div className='pt-10'>
                 <Link  to="contact"  smooth={true}  duration={500} >                
                 <button className='text-white group border-2 px-6 py-3 my-2  flex items-center hover:bg-[#D5A82C] hover:border-[#D5A82C] hover:text-black'>       
